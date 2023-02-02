b9228cbc959af6f13e707e1594e343f7a6ee24ba
wishabc <aswq22013@gmail.com>
2023-02-02 13:21:04 -0800

    extract context process changed window

--- bin/filter_variants.py
+++ bin/filter_variants.py
@@ -2,13 +2,11 @@ import pandas as pd
 import sys
 
 
-def main():
-
+def main(bed_file):
+    pass
 
 if __name__ == '__main__':
-    bed_file = pd.read_table(sys.argv[1], header=None,
-        names=['chr', 'start', 'end', 'ref', 'alt',
-            'mut_rates_roulette', 'mut_rates_gnomad',
-            'chr1', 'start1', 'end1', 'ref', 'alt'
-            ])
-    main()
+    bed_file = pd.read_table(sys.argv[1])
+    print(bed_file.columns)
+    exit(1)
+    main(bed_file)
