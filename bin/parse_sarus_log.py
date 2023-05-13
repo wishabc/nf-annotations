03a0300a3031c71e674a518fbe12e38bb474970c
wishabc <aswq22013@gmail.com>
2023-05-13 16:16:34 -0700

    added rs id to id

--- bin/parse_sarus_log.py
+++ bin/parse_sarus_log.py
@@ -1,11 +1,11 @@
 import sys
 import pandas as pd
-import os
 from parse_variants_motifs import read_pfm
 from tqdm import tqdm
+import pyfaidx
 
 
-def choose_best(data, motif_id, tr):
+def choose_best(data, motif_id, motif_length, fasta, tr):
     key, value = data
     chrom, end, rs_id, ref, alt = key.split('@')
     end = int(end)
@@ -14,16 +14,17 @@ def choose_best(data, motif_id, tr):
         if (x[0] != best_allele) and (x[2] == best_pos) and (x[3] == best_orient)]
     assert len(res) == 1
     _, other_pval, *_ = res[0]
-    stats = [best_pos, 1 if best_pval >=tr else 0, best_orient]
+    stats = [best_pos, 1 if best_pval >= tr else 0, best_orient]
     if best_allele == 'ref':
         stats.extend([best_pval, other_pval])
     else:
         stats.extend([other_pval, best_pval])
+    start = end - 1
+    seq = fasta[chrom][start - best_pos:end + motif_length - best_pos]
+    return [chrom, start, end, rs_id, ref, alt, motif_id, *stats, seq]
 
-    return [chrom, end - 1, end, rs_id, ref, alt, motif_id, *stats]
 
-
-def main(sarus_log, motif_length, window_size, out_file, motif_path, tr=4):
+def main(sarus_log, fasta_path, motif_length, window_size, out_file, motif_path, tr=4):
     result = {}
     
     motif_id, pfm = read_pfm(motif_path)
@@ -45,13 +46,15 @@ def main(sarus_log, motif_length, window_size, out_file, motif_path, tr=4):
                     continue
                 motif_score = float(motif_score)
                 result.setdefault(key, []).append([allele, motif_score, motif_pos, motif_orient])
-
-    pd.DataFrame([choose_best(x, motif_id, tr) for x in tqdm(result.items())], 
-        columns=['#chr', 'start', 'end', 'ID', 'ref', 'alt', 'motif',
-                'motif_pos', 'signif_hit', 'motif_orient', 'motif_logpval_ref', 'motif_logpval_alt',]
-        ).to_csv(out_file, sep='\t', index=False)
+    with pyfaidx.Fasta(fasta_path, sequence_always_upper=True) as fasta:
+        pd.DataFrame([choose_best(x, motif_id, motif_length, fasta, tr=tr) for x in tqdm(result.items())], 
+            columns=['#chr', 'start', 'end', 'ID', 'ref', 'alt', 'motif',
+                    'motif_pos', 'signif_hit', 'motif_orient',
+                    'motif_logpval_ref', 'motif_logpval_alt', 'seq']
+            ).to_csv(out_file, sep='\t', index=False)
             
 
 
 if __name__ == '__main__':
-    main(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]), sys.argv[4], sys.argv[5])
+    main(sys.argv[1], sys.argv[2], int(sys.argv[3]),
+        int(sys.argv[4]), sys.argv[5], sys.argv[6])
