04706a9b3b37c060450218c1a38e8c80fb1cef29
wishabc <aswq22013@gmail.com>
2023-08-09 15:58:54 -0700

    added params.gtffiles

--- bin/reformat_sumstats.py
+++ bin/reformat_sumstats.py
@@ -2,20 +2,25 @@ import pandas as pd
 import sys
 import numpy as np
 
+result_columns = ['#chr', 'start', 'end', 'ref', 'alt', 'Beta', 'Beta_se', 'P', 'neglog10_p']
 
 def main(df):
-    df['P'] = np.power(10, -df['neglog10_pval_meta_hq'])
+    try:
+        df['P'] = np.power(10, -df['neglog10_pval_EUR'])
+    except KeyError:
+        return pd.DataFrame([], columns=result_columns)
     df['#chr'] = "chr" + df['chr']
     df['start'] = df['pos'] - 1
     df.rename(
         columns={
             'pos': 'end',
-            'beta_meta_hq': 'Beta',
-            'se_meta_hq': 'Beta_se',
-            'neglog10_pval_meta_hq': 'neglog10_p'
-        }, inplace=True
+            'beta_EUR': 'Beta',
+            'se_EUR': 'Beta_se',
+            'neglog10_pval_EUR': 'neglog10_p'
+        }, 
+        inplace=True
     )
-    return df[['#chr', 'start', 'end', 'ref', 'alt', 'Beta', 'Beta_se', 'P', 'neglog10_p']]
+    return df[result_columns]
 
 if __name__ == '__main__':
     phen_df = pd.read_table(sys.argv[1], compression='gzip')
