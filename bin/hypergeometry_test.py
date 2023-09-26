6cd7d8d16c40c4349b3580363ad40f309d015bee
anas fathul <muhdanas@uw.edu>
2023-09-26 12:46:19 -0700

    delete unnecessary function

--- bin/hypergeometry_test.py
+++ bin/hypergeometry_test.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 import pandas as pd
 import numpy as np
 import scipy.stats as stats
@@ -42,49 +41,6 @@ def calculate_logodd(col):
     log_or = np.log(OR)
     
     return log_or
-
-# Function to aggregate p-value
-def cauchy_combination(p_val):
-    return stats.cauchy.sf(np.sum(np.tan(np.multiply(np.subtract(0.5, p_val), np.pi))) / len(p_val))
-
-# Function to generate plot
-def generate_top(modal, cluster, component, **kwargs):
-    X_components = 'X' + str(component)
-    vis = modal[modal["Components"] == X_components].head(10).sort_values(by="Estimation")
-    # vis.plot(x="tf_name", y="Estimation", kind="bar")
-    colors_order = get_colors_order(component)
-    plt.barh(data=vis, y=cluster, width='Estimation', color = colors_order[component - 1] ,  **kwargs)
-    plt.title(X_components)
-    file_name = X_components + '.png'
-    plt.savefig(file_name, bbox_inches='tight')
-    plt.show()
-
-def merge_aggregate_fdr(all_coeffs, motif_meta_df):
-    # combine coeffs with some columns with motifs_metadata
-    new_df = pd.merge(all_coeffs, motif_meta_df[['motif_id','tf_name']], on='motif_id', how='left')
-    # Cauchy aggregation
-    qval_df = new_df.pivot_table(index='tf_name', columns='Unnamed: 0',
-                             values='Pr(>|z|)', aggfunc=cauchy_combination)
-    # Do fdr corrected on p-val, fdr_by
-    qval_df_fdr = qval_df.applymap(lambda x: multipletests(x, alpha=0.05,
-                                                           method='fdr_by', maxiter=1)[1][0])
-    # Average estimate based on tf_name
-    tf_name_est_df = new_df.pivot_table(index='tf_name',
-                                        columns='Unnamed: 0',values='estimate', aggfunc=np.mean)
-    # Indexing based on estimation
-    pvaltest_df = pd.DataFrame({'index': np.argmax(tf_name_est_df.to_numpy(), axis=1),
-                                'value': tf_name_est_df.max(axis=1)})
-    max_df = pvaltest_df.sort_values(by=['index', 'value'], ascending=[True, False])
-    # Lock intended index on df for masking
-    est = tf_name_est_df.loc[max_df.index, :]
-    qvals = qval_df_fdr.loc[max_df.index, :]
-    # set masking and return long format of sorted estimation by components
-    filtered_est = est.where(~((est<0) | (qvals>0.05)))
-    filtering = filtered_est.reset_index()
-    tf_result_cluster_name = filtering.melt('tf_name',
-                                            var_name='Components',
-                                            value_name='Estimation').sort_values('Estimation', ascending=False)
-    return tf_result_cluster_name
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='All_coeffs file to components')
