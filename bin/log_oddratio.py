1420fd715e270f350868fd5295ce1ea3f8b52ffd
anas fathul <muhdanas@uw.edu>
2023-12-17 17:59:05 -0800

    Cleaning rscript

--- bin/log_oddratio.py
+++ bin/log_oddratio.py
@@ -47,15 +47,15 @@ def log_odd_ratio_np(ori_matrix_np, indicator_df):
     #log_odd_rat = np.log((a * d) / (b * c))
     log_odd_rat = np.where((b * c) != 0, np.log((a * d) / (b * c)), 0)
     
-    M = a + b + c + d
-    n = a + c
-    N = a + b
+    #M = a + b + c + d
+    #n = a + c
+    #N = a + b
     
     p_values = np.zeros_like(log_odd_rat)
     
     # later check for case where a == expected, whether this is taken care by python
-    for i in range(len(a)):
-        p_values[i] = 1 - hypergeom.cdf(a[i]-1, M[i], n[i], N[i])
+    #for i in range(len(a)):
+    #    p_values[i] = 1 - hypergeom.cdf(a[i]-1, M[i], n[i], N[i])
     
     return log_odd_rat, p_values
 
@@ -65,22 +65,15 @@ if __name__ == '__main__':
     parser.add_argument('indicator', help='Path to indicator file')
     args = parser.parse_args()
 
-    # matrix_original_df = pd.DataFrame(np.load('/net/seq/data2/projects/afathul/motif_enrichment/odd_ratio/nmf_32.H.npy').T)
-    # matrix_original_df.columns = ['comp_' + str(i) for i in list(range(1, len(matrix_original_df.columns) + 1))]
-    # Find maximum value in each row and mark as 1, others as 0
-    # binary_nmf_matrix = matrix_original_df.eq(matrix_original_df.max(axis=1), axis=0).astype(int)
-
     matrix_original = np.load('/net/seq/data2/projects/afathul/motif_enrichment/odd_ratio/original_binary_matrix.npy').T
-    # sample_metadata = pd.read_table('/net/seq/data2/projects/afathul/motif_enrichment/odd_ratio/sample_metadata_new.tsv')
-    # matrix_original_df.columns = sample_metadata['ag_id'].values
     indicator_matrix = pd.read_csv(args.indicator, header=None).values
 
     print('load data done')
-    # result_df = log_odd_ratio(binary_nmf_matrix, indicator_df).reset_index()
+
     logodd, pval = log_odd_ratio_np(matrix_original, indicator_matrix)
     print('log_odd_np done')
     motif_id_name = args.motif_id
-    # result_df['motif_id'] = motif_id_name
+
 
     # will output one file only
     #result_df.to_csv(motif_id_name + ".coeff.tsv", sep='\t', index=False)
