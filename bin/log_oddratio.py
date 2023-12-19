3ee67b7c915abe2fa314663ca53148679f30377d
anas fathul <muhdanas@uw.edu>
2023-12-19 13:49:03 -0800

    update on output

--- bin/log_oddratio.py
+++ bin/log_oddratio.py
@@ -76,8 +76,8 @@ if __name__ == '__main__':
     print('log_odd_np done')
     motif_id_name = args.motif_id
 
-    result_logodd_df = pd.DataFrame(np.append(motif_id_name, logodd))
-    result_pval_df = pd.DataFrame(np.append(motif_id_name, pval))
+    result_logodd_df = pd.DataFrame(np.append(motif_id_name, logodd).reshape(-1, len(np.append(motif_id_name, logodd))))
+    result_pval_df = pd.DataFrame(np.append(motif_id_name, pval).reshape(-1, len(np.append(motif_id_name, pval))))
 
     result_logodd_df.to_csv(motif_id_name + ".logodd.tsv", sep='\t', index=False)
     result_pval_df.to_csv(motif_id_name + ".pval.tsv", sep='\t', index=False)
