b0a8090b1bdb0ede5bbe16cc0916a927223b27f2
anas fathul <muhdanas@uw.edu>
2024-01-22 18:01:27 -0800

    sample odds ratio

--- bin/log_oddratio.py
+++ bin/log_oddratio.py
@@ -5,37 +5,15 @@ from scipy.stats import fisher_exact
 from scipy.stats import hypergeom
 import argparse # using argparse for args
 print('past the import stuff')
-def log_odd_ratio(ori_df, indicator_df):
+def sample_odds(ori_matrix_np, indicator_df):
+    print('inside function')
+    a = (ori_matrix_np * indicator_df).sum(axis=0) # (3883,)
+    c = ori_matrix_np.sum(axis=0) - a # (3883,)
     
-    log_odds_ratios = []
-    pval_fischer = []
-    
-    for col in ori_df.columns:
-        # motif=1, sample=1
-        a = (ori_df[col] & indicator_df.iloc[:, 0]).sum()
-        # motif=1, sample=0
-        b = indicator_df.iloc[:, 0].sum() - a
-        # motif=0, sample=1
-        c = ori_df[col].sum() - a
-        # motif=0, sample=0
-        d = (len(ori_df[col]) - ori_df[col].sum()) - b
-        
-        # Calculate the log odds ratio
-        log_odds_ratios.append(np.log((a * d) / (b * c)))
-        
-        # Compute p-value
-        try:
-            _, p_value = fisher_exact(np.array([[a, b], [c, d]]))
-        except ValueError:
-            print(a, b, c, d)
-            raise
+    result_value = a / c
+    pval = 0
 
-        pval_fischer.append(p_value)
-        
-
-    return pd.DataFrame({'log_odd_ratio' : log_odds_ratios,
-                        'fischer_pval' : pval_fischer},
-                        index=ori_df.columns)
+    return result_value, pval
 
 def log_odd_ratio_np(ori_matrix_np, indicator_df):
     print('inside function')
@@ -76,7 +54,9 @@ if __name__ == '__main__':
 
     print('load data done')
 
-    logodd, pval = log_odd_ratio_np(matrix_original, indicator_matrix)
+    #logodd, pval = log_odd_ratio_np(matrix_original, indicator_matrix)
+    logodd, pval = sample_odds(matrix_original, indicator_matrix)
+
     print('log_odd_np done')
     motif_id_name = args.motif_id
     id_name = pd.read_table(args.meta_data, usecols=['ag_id'])
