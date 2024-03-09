b535ada2328b9f9493a263dd4ad351813d8282fb
wishabc <aswq22013@gmail.com>
2024-03-09 03:50:50 -0800

    Refactor merge_variants.py to include header in hg38_variants

--- bin/merge_variants.py
+++ bin/merge_variants.py
@@ -4,11 +4,13 @@ import sys
 
 if __name__ == '__main__':
     initial_variants = pd.read_table(sys.stdin).set_index('varid')
+    index = initial_variants.index.copy()
+
     hg38_variants = pd.read_table(
-        sys.argv[1], 
+        sys.argv[1], header=None,
         names=['hg38_chr', 'hg38_start', 'hg38_end', 'varid']
     ).set_index('varid')
-    index = hg38_variants.index.copy()
+    
     initial_variants.join(hg38_variants, how='left')[index].reset_index()[
         ['hg38_chr', 'hg38_start', 'hg38_end', 'varid', *initial_variants.columns]
     ].to_csv(
