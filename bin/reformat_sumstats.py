f601e349bd4d8058931e5c88ecd0b9d1adc9267d
wishabc <aswq22013@gmail.com>
2024-03-09 20:08:54 -0800

    Refactor reformat_sumstats.py to improve readability and maintainability

--- bin/reformat_sumstats.py
+++ bin/reformat_sumstats.py
@@ -5,8 +5,12 @@ import numpy as np
 result_columns = ['#chr', 'start', 'end', 'SNP', 'ref', 'alt', 'Beta', 'Beta_se', 'P', 'neglog10_p', 'INFO', 'phen_id', 'N']
 
 def main(df, population):
-    assert np.all(df['varid'] == df['chr'] + ':' + df['pos.1'].astype(str) + '_' + df['ref.1'] + '_' + df['alt.1'])
-    assert np.all(df['varid'] == df['chrom'] + ':' + df['pos'].astype(str) + '_' + df['ref'] + '_' + df['alt'])
+    assert np.all(
+        (df['chrom'] == df['chr']) & 
+        (df['pos'] == df['pos.1']) & 
+        (df['ref'] == df['ref.1']) & 
+        (df['alt'] == df['alt.1'])
+    )    
     try:
         df['P'] = np.power(10, -df[f'neglog10_pval_{population}'])
     except KeyError:
@@ -32,7 +36,9 @@ def main(df, population):
         inplace=True
     )
     df['#chr'] = 'chr' + df['#chr']
-    return df.dropna(subset=['Beta', 'neglog10_p'])
+    df.dropna(subset=['Beta', 'neglog10_p', 'hg38_start'], inplace=True)
+    df[['start', 'end']] = df[['start', 'end']].astype(int)
+    return df
 
 if __name__ == '__main__':
     phen_df = pd.read_table(sys.stdin, dtype={'chr': str, 'chrom': str})
