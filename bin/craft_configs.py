edaa381f7ebeb7774054be6c84278501ae4dca10
wishabc <aswq22013@gmail.com>
2024-08-27 07:23:15 -0700

    Refactor craft_configs.py to update variable names and use 'SAMPLE_NAMES' instead of 'sample_names'

--- bin/craft_configs.py
+++ bin/craft_configs.py
@@ -6,7 +6,7 @@ import pandas as pd
 def main(metadata, samples_meta_path, outdir):
     for _, row in metadata.iterrows():
         config = configparser.ConfigParser()
-        prefix = row['prefix']
+        prefix = f"{row['prefix']}.{row['n_components']}"
         base_path = f"{outdir}/mixing/{prefix}/{prefix}"
         # Add sections and settings
         config['METADATA'] = {
