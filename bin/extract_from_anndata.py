662b709db71cda2727f8afe78a41154d6e6eaeab
wishabc <aswq22013@gmail.com>
2025-01-07 20:23:48 +0700

    Update extract_from_anndata.py to adjust output file parameters for binary matrix and observation names

--- bin/extract_from_anndata.py
+++ bin/extract_from_anndata.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
     peaks_mask = np.loadtxt(sys.argv[2], dtype=bool)
     anndata = read_zarr_backed(sys.argv[1])[:, peaks_mask]
     binary_matrix = anndata.layers['binary'].todense().T
-    np.save(sys.argv[2], binary_matrix)
-    np.savetxt(sys.argv[3], anndata.obs_names, fmt='%s')
-    anndata.var.to_csv(sys.argv[4])
+    np.save(sys.argv[3], binary_matrix)
+    np.savetxt(sys.argv[4], anndata.obs_names, fmt='%s')
+    anndata.var.to_csv(sys.argv[5])
