a422046ce65cffd8bf77bcc7f054b9266f7d4472
wishabc <aswq22013@gmail.com>
2025-01-07 19:00:33 +0700

    bug masterlist is now extracted

--- bin/extract_from_anndata.py
+++ bin/extract_from_anndata.py
@@ -7,3 +7,4 @@ if __name__ == "__main__":
     binary_matrix = anndata.layers['binary'][:, :].todense().T
     np.save(sys.argv[2], binary_matrix)
     np.savetxt(sys.argv[3], anndata.obs_names, fmt='%s')
+    anndata.var.to_csv(sys.argv[4])
