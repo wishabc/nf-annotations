aad9ca7b5fefe0550f8d0a2d7aef199a14edec18
wishabc <aswq22013@gmail.com>
2025-03-29 01:47:47 +0800

    fix: update sampling logic to return a list of indices and improve progress tracking

--- bin/gwas_enrichment/sample.py
+++ bin/gwas_enrichment/sample.py
@@ -1,6 +1,8 @@
 import sys
 import pandas as pd
+from tqdm import tqdm
 
+tqdm.pandas()
 
 matching_cols = ['tss_bin', 'ld_bin', 'maf_bin']
 
@@ -9,15 +11,19 @@ def sample_random(df: pd.DataFrame, count_to_sample: pd.Series, seed):
     if df.name not in count_to_sample.index:
         return []
     n = count_to_sample.loc[df.name]
-    return df.sample(n=n, random_state=seed).index
+    return df.sample(n=n, random_state=seed).index.tolist()
 
 
 def main(ref_pop_df: pd.DataFrame, count_to_sample, seed):
-    sampled = ref_pop_df[matching_cols].groupby(matching_cols).apply(
-        sample_random,
-        count_to_sample=count_to_sample,
-        seed=seed
-    )
+    sampled_indices = []
+    grouped = ref_pop_df.groupby(matching_cols)
+
+    for name, group in tqdm(grouped, total=len(grouped)):
+        if name in count_to_sample.index:
+            n = count_to_sample.loc[name]
+            sampled = group.sample(n=n, random_state=seed).index
+            sampled_indices.extend(sampled)
+
     return ref_pop_df.loc[sampled.values, ['chrom', 'start', 'end', *matching_cols]].sort_values(['chrom', 'start'])
 
 
