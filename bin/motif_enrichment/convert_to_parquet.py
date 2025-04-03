f2f5b5e6707d3d72b3b342c43042edcf8f173eb6
wishabc <aswq22013@gmail.com>
2025-04-03 23:10:15 +0700

    fix: add data reading and writing messages in convert_to_parquet.py

--- bin/motif_enrichment/convert_to_parquet.py
+++ bin/motif_enrichment/convert_to_parquet.py
@@ -1,13 +1,15 @@
 import pandas as pd
 import sys
 
-
+print('Reading data...')
 df = pd.read_table(sys.argv[1], names=['#chr', 'start', 'end', 'length', 'n_gc', 'gc', 'gc_bin', 'length_bin']).drop(columns=['n_gc', 'length', 'gc'])
 
 df['#chr'] = df['#chr'].astype('category')
 df['gc_bin'] = df['gc_bin'].astype('category')
 df['length_bin'] = df['length_bin'].astype('category')
+df = df.sort_values(["gc_bin", "length_bin"])
 
+print('Writing data...')
 df.to_parquet(
     sys.argv[2],
     index=False,
