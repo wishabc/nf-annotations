119a421037c74c9a40bd1c910b1d92b8e95c3800
wishabc <aswq22013@gmail.com>
2025-06-09 21:59:19 +0700

    refactor filter_na.py to define matching_cols variable for improved readability

--- bin/gwas_enrichment/filter_na.py
+++ bin/gwas_enrichment/filter_na.py
@@ -2,13 +2,15 @@ import pandas as pd
 import sys
 
 
+matching_cols = ['tss_bin', 'ld_bin', 'maf_bin']
+
 if __name__ == '__main__':
     header = pd.read_table(sys.argv[1], nrows=0).columns.tolist()
     df = pd.read_table(sys.stdin, header=None, names=header).dropna(
         subset=[
             'tss_bin', 'ld_bin', 'maf_bin'
         ]
-    )
+    )[['#chr', 'start', 'end', *matching_cols, 'rs_id']]
     df['sampling_type'] = 'ref'
 
     df.to_csv(sys.stdout, sep='\t', index=False, header=True)
